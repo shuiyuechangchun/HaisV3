ead826307a3b1bab7ea7f57d90f753a6284ee4be
Hello_海生 <hais1992@163.com>
2021-11-01 22:00:14 +0800

    21.11.01,升级面具,修复boot处理

--- Bin/0.Install.sh
+++ Bin/0.Install.sh
@@ -16,22 +16,6 @@ echo "
 
 ${su} apt update
 ${su} apt upgrade -y
-${su} apt install -y git
-${su} apt install -y cpio
-${su} apt install -y bc 
-${su} apt install -y aria2 
-${su} apt install -y brotli 
-${su} apt install -y android-sdk-libsparse-utils 
-${su} apt install -y openjdk-11-jre 
-${su} apt install -y p7zip-full 
-${su} apt install -y curl 
-${su} apt install -y python3 
-${su} apt install -y python3-pip 
-${su} apt install -y zipalign 
-${su} apt install -y zip 
-${su} apt install -y unzip 
-${su} apt install -y img2simg 
-${su} apt install -y dos2unix 
-${su} apt install -y jq
+${su} apt install -y git cpio bc file aria2 brotli android-sdk-libsparse-utils openjdk-11-jre p7zip-full curl python3 python3-pip zipalign zip unzip img2simg dos2unix jq
 #${su} pip3 install aliyunpan
 ${su} pip3 install -r $SHELL_PATH/Lib/extract_android_ota_payload/requirements.txt
