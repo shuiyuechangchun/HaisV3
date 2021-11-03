a76e454a70b05e25b25459f12b59ff0db969e860
Hello_海生 <hais1992@163.com>
2021-11-03 15:00:48 +0000

    update Bin/0.ArmInstall.sh.

--- Bin/0.ArmInstall.sh
+++ Bin/0.ArmInstall.sh
@@ -142,7 +142,6 @@ init() {
 
 init &> /dev/null
 
-echo -en '\n\n系统安装完成\n\n工具所在目录为 $(pwd)/HaisV3\n\n请重启后继续通过下面的命令安装HaisV3工具\n\n./install.sh\n\n'
+debian
 
-curl https://gitee.com/hais/HaisV3/raw/master/Bin/0.NetInstall.sh -o "$HOME/$linux/root/install.sh" &> /dev/null
-chmod 0777 "$HOME/$linux/root/install.sh" &> /dev/null
+bash <(curl -s https://gitee.com/hais/HaisV3/raw/master/Bin/0.NetInstall.sh)
